bef44bc07b697525ead839c9e7e1a8e116d7e0c4
root <dmuv7@hotmail.com>
2017-12-29 14:30:35 +0100

    carpeta jenkins actualizada

--- jenkins/script-to-execute.sh
+++ jenkins/script-to-execute.sh
@@ -4,7 +4,7 @@ clear
 	echo "Se pone en marcha jenkins"
 	nohup ./usr/local/bin/jenkins.sh &
 	
-	sleep 60
+	sleep 240
 	echo "Se ejecuta el pipeline"
 	jenkins-jobs --conf /etc/jenkins_jobs/jenkins_jobs.ini update /etc/jenkins_jobs/scm_pipeline.yaml
 
